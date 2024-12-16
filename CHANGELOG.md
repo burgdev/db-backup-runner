9d589b3c5b5f04970046c38467c58e4ff34f76d3
Tobias Burgherr <tburgherr@gmail.com>
2024-12-16 13:22:17 +0100

    Added release flow

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,28 @@
-# Changelog
+## 0.1.3
 
-All notable changes to this project will be documented in this file.
-This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+### Other changes
+
+- Add github docker build action ([#5](https://github.com/burgdev/db-backup-runner/pull/5))
+- Command to automatically create a restore script. ([#1](https://github.com/burgdev/db-backup-runner/pull/1))
+- Feat/gh action docker ([#3](https://github.com/burgdev/db-backup-runner/pull/3))
+- Improved restore script ([#2](https://github.com/burgdev/db-backup-runner/pull/2))
+
+## 0.1.2
+
+<!-- No changes -->
+
+<!-- No changes -->
+
+<!-- No changes -->
+
+<!-- No changes -->
+
+<!-- No changes -->
+
+<!-- No changes -->
+
+<!-- No changes -->
+
+<!-- No changes -->
+
+# Changelog
--- tasks/project.py
+++ tasks/project.py
@@ -29,3 +29,27 @@ def install(c: Ctx, venv_update: bool = True):
     if venv_update:
         update_venv(c)
     success("Installation done, your are ready to go ...")
+
+
+@task(help={"force": "Force tag creation and pushing"})
+def release(c: Ctx, force: bool = True):
+    """Prepare a release, update CHANGELOG file and bump versions"""
+    echo("🚀 Creating virtual environment using pyenv and poetry")
+    out = c.run("rooster release")
+    last_tag = None
+    new_tag = None
+    for line in out.stdout.split("\n"):
+        if "last version tag" in line:
+            last_tag = line.split(" ")[-1]
+        elif "new version" in line:
+            new_tag = line.split(" ")[-1]
+    if new_tag:
+        success(
+            f"Bump from version '{last_tag}' to '{new_tag}'."
+        ) if last_tag else success(f"Bump to version '{new_tag}'.")
+        if input(f"Create new tag 'v{new_tag}' [N/y]: ").lower() in ["yes", "y"]:
+            c.run(f"git tag -f 'v{new_tag}'")
+            if input("Push tags [N/y]: ").lower() in ["yes", "y"]:
+                c.run(f"git push origin 'v{new_tag}'")
+    else:
+        warning("Did not update to new version tag.")
