e81b57cd34415df12f5fd7f65ece5ba2634e8a5d
Tobias Burgherr <tobias.burgherr@haag-streit.com>
2025-09-15 19:14:57 +0200

    Add DATABASE into dump command

--- src/db_backup_runner/provider/_postgres.py
+++ src/db_backup_runner/provider/_postgres.py
@@ -1,6 +1,3 @@
-import click
-
-
 from db_backup_runner.provider import BackupProviderBase
 
 
@@ -10,7 +7,7 @@ class PostgresBackupProvider(BackupProviderBase):
     name = "postgres"
     default_dump_binary = "pg_dump"
     # default_dump_binary = "pg_dumpall"
-    default_dump_args = "-Fc -U USER"
+    default_dump_args = "-Fc -U USER DATABASE"
     default_restore_binary = "pg_restore"
     default_restore_args = "-Fc -U USER -d DATABASE"
     plain_file_extension = ".dump"
@@ -18,18 +15,28 @@ class PostgresBackupProvider(BackupProviderBase):
     def dump(self) -> str:
         """Overwrite dump method with custom postgres dump"""
         env = self.get_container_env()
-        user = env.get("POSTGRES_USER") or "postgres"
+        user = self.get_container_label("user", env.get("POSTGRES_USER") or "postgres")
+        assert user is not None
+        database_env = (
+            env.get("POSTGRES_DATABASE", env.get("POSTGRES_DB")) or "postgres"
+        )
+        database = self.get_container_label("database", database_env)
+        assert database is not None
 
-        return f"{self.get_dump_binary()} {self.get_dump_args()}".replace("USER", user)
+        return f"{self.get_dump_binary()} {self.get_dump_args()}".replace(
+            "USER", user
+        ).replace("DATABASE", database)
 
     def get_restore_args(self) -> str:
         """Overwrite restore args with custom postgres arguments"""
         env = self.get_container_env()
-        user = env.get("POSTGRES_USER") or "postgres"
-        # TODO get database from label or argument
-        database = env.get("POSTGRES_DATABASE") or click.style(
-            "DATABASE", fg="green", bold=True
+        user = self.get_container_label("user", env.get("POSTGRES_USER") or "postgres")
+        assert user is not None
+        database_env = (
+            env.get("POSTGRES_DATABASE", env.get("POSTGRES_DB")) or "postgres"
         )
+        database = self.get_container_label("database", database_env)
+        assert database is not None
         return (
             (self.get_container_label("restore_args", self.default_restore_args) or "")
             .replace("USER", user)
